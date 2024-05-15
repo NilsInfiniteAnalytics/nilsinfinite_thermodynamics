b6a635411145091a39ade942cf18993b866532f3
Christopher Popovich <c.a.popovich@gmail.com>
2024-05-14 21:00:07 -0400

    Add molar mass ratio constant.

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ pub const MOLAR_MASS_WATER: f64 = 0.01801528; // kg mol-1
 // Derived parameters
 pub const R_D: f64 = GAS_CONSTANT / MOLAR_MASS_DRY_AIR;
 pub const R_V: f64 = GAS_CONSTANT / MOLAR_MASS_WATER;
+pub const DRY_AIR_TO_WATER_MOLAR_MASS_RATIO: f64 = MOLAR_MASS_DRY_AIR / MOLAR_MASS_WATER;
 
 #[cfg(test)]
 mod tests {
@@ -15,13 +16,15 @@ mod tests {
     #[test]
     fn test_derived_parameters() {
         // Expected values
-        let expected_r_d = 287.06; // J kg-1 K-1
-        let expected_r_v = 461.52; // J kg-1 K-1
+        let expected_r_d = 287.061863; // J kg-1 K-1
+        let expected_r_v = 461.522652; // J kg-1 K-1
+        let expected_molar_mass_ratio = 1.60774;
 
         // Allow a small tolerance for floating-point comparisons
-        let tolerance = 1e-2;
+        let tolerance = 1e-3;
 
         assert!((R_D - expected_r_d).abs() < tolerance);
         assert!((R_V - expected_r_v).abs() < tolerance);
+        assert!((DRY_AIR_TO_WATER_MOLAR_MASS_RATIO - expected_molar_mass_ratio).abs() < tolerance);
     }
 }
