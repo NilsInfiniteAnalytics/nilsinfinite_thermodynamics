cdcf1f25d7a53bf94e20a2627c0986579ab73572
Christopher Popovich <c.a.popovich@gmail.com>
2024-05-14 20:51:47 -0400

    Added initial constants and derived parameters.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,27 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+pub const STANDARD_PRESSURE: f64 = 101325.0; // Pa
+pub const STANDARD_TEMPERATURE: f64 = 273.15; // K
+pub const GAS_CONSTANT: f64 = 8.3144598; // J mol-1 K-1
+pub const MOLAR_MASS_DRY_AIR: f64 = 0.028964; // kg mol-1
+pub const MOLAR_MASS_WATER: f64 = 0.01801528; // kg mol-1
+
+// Derived parameters
+pub const R_D: f64 = GAS_CONSTANT / MOLAR_MASS_DRY_AIR;
+pub const R_V: f64 = GAS_CONSTANT / MOLAR_MASS_WATER;
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_derived_parameters() {
+        // Expected values
+        let expected_r_d = 287.06; // J kg-1 K-1
+        let expected_r_v = 461.52; // J kg-1 K-1
+
+        // Allow a small tolerance for floating-point comparisons
+        let tolerance = 1e-2;
+
+        assert!((R_D - expected_r_d).abs() < tolerance);
+        assert!((R_V - expected_r_v).abs() < tolerance);
     }
 }
